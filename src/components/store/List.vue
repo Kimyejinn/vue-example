41554c9333899d33db30dd93b46ed293aa0906fc
Kimyejinn <kimyejin2908@naver.com>
2022-05-12 16:44:16 +0900

    store list add and remove

--- src/components/store/List.vue
+++ src/components/store/List.vue
@@ -1,34 +1,46 @@
 <template>
 	<h2>List</h2>
 	<div>
-		<span>text : <input type="text" v-model="text" /></span>&nbsp;
+		<span>text : <input type="text" ref="focusText" v-model="text" /></span>&nbsp;
 		<button @click="add">추가</button>
 	</div>
 	<ul style="text-align: left">
-		<li v-for="(i, index) in datas" :key="index">{{ i.id }} - {{ i.text }}</li>
+		<li v-for="(i, index) in datasArr" :key="index"><button @click="remove(i.id)">X</button>&nbsp;{{ i.id }} - {{ i.text }}</li>
 	</ul>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { computed, ref, watch } from 'vue';
 import { useStore } from 'vuex';
 export default {
 	name: 'list',
 	setup() {
 		const store = useStore();
+		const datasObj = computed(() => store.state.List.obj);
+		const datasArr = computed(() => store.state.List.arr);
 
-		const datas = ref([
-			{ id: 1, text: 'one' },
-			{ id: 2, text: 'two' },
-			{ id: 3, text: 'three' },
-		]);
 		const text = ref('');
+		const focusText = ref(null);
 
 		const add = () => {
-			alert('add click' + text.value);
+			if (text.value != '') {
+				var data = {
+					id: new Date().getTime().toString(36),
+					text: text.value,
+				};
+
+				store.dispatch('List/add', data);
+			} else {
+				alert('text를 입력해주세요');
+				focusText.value.focus();
+			}
 		};
 
-		return { add, datas, text };
+		const remove = id => {
+			store.dispatch('List/remove', id);
+		};
+
+		return { add, remove, text, datasObj, datasArr, focusText };
 	},
 };
 </script>
--- src/store/modules/List.js
+++ src/store/modules/List.js
@@ -1,11 +1,27 @@
 export const List = {
 	namespaced: true,
-	state: { counter: 10 },
+	state: { obj: {}, arr: [] },
 	getters: {},
 	mutations: {
 		//동기
+		insert(state, data) {
+			state.obj[data.id] = {
+				id: data.id,
+				text: data.text,
+			};
+			state.arr.push(state.obj[data.id]);
+		},
+		delete(state, data) {
+			state.arr = state.arr.filter(d => d.id !== data);
+		},
 	},
 	actions: {
 		//비동기
+		add({ commit }, data) {
+			commit('insert', data);
+		},
+		remove({ commit }, data) {
+			commit('delete', data);
+		},
 	},
 };
